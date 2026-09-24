5b40794c3656a657d6b9bf3800addd13690c85e0
anshulbadoni360 <anshul.badoni@ashmar.in>
2026-09-24 19:55:23 +0530

    more ui redesign 3

--- lib/features/detail/detail_screen.dart
+++ lib/features/detail/detail_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 import 'package:palette_generator/palette_generator.dart';
 
